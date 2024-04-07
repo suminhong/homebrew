af35aec135a98f52c0100c669376d23e1e41f8d4
suminhong <honglab97@gmail.com>
2024-04-08 00:12:56 +0900

    gohonglab

---
+++ gohonglab.rb
@@ -0,0 +1,11 @@
+class Gohonglab < Formula
+    desc "honglab 간단한 명령줄"
+    homepage "https://github.com/suminhong/gohonglab"
+    url "https://github.com/suminhong/gohonglab/releases/download/v1.0.0/gohonglab"
+    sha256 "77cf0d454c7267cb9b929e17a33a5b13bedde026ed25c83d132a74f40d7b6e52"
+  
+    def install
+      bin.install "gohonglab"
+    end
+  end
+  
